64366a2e65f11734da4cc167a9d7bac2950cb714
Alice Kang <43328684+atykwonderland@users.noreply.github.com>
2024-03-26 15:12:29 -0700

    implement frontend auth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,13 +12,17 @@ import CreateBook from "./components/CreateBook";
 import ShowBookList from "./components/ShowBookList";
 import ShowBookDetails from "./components/ShowBookDetails";
 import UpdateBookInfo from "./components/UpdateBookInfo";
+import { Home, Login, Signup } from "./pages";
 
 // Routes
 const router = createBrowserRouter([
-  { path: "/", element: <ShowBookList /> },
+  // { path: "/", element: <ShowBookList /> },
+  { path: "/", element: <Home /> },
   { path: "/create-book", element: <CreateBook /> },
   { path: "/show-book/:id", element: <ShowBookDetails /> },
   { path: "/edit-book/:id", element: <UpdateBookInfo /> },
+  { path: "/signup", element: <Signup /> },
+  { path: "/login", element: <Login /> },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
